35d43d4c0a62331783291b009528d9fb26c16b24
Boyd Tiffin <mokono@fullsail.edu>
2013-10-31 08:37:57 -0500

    Wed night upload

--- Tiffin_boyd_project1/js/json.js
+++ Tiffin_boyd_project1/js/json.js
@@ -14,5 +14,150 @@ var json = {
 		"description" : ["Description:", "Test Description 2"],
 		"status" : ["Status:", "Requirements"],
 		"type" : ["Type:", "website"]
+	},
+	"idea3": {
+		"title" : ["Idea Title:", "Idea 1"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-11-11"],
+		"description" : ["Description:", "Test Description 1"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "domain"]
+	},
+	"idea4": {
+		"title" : ["Idea Title:", "Idea 2"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-12-12"],
+		"description" : ["Description:", "Test Description 2"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "website"]
+	},
+	"idea5": {
+		"title" : ["Idea Title:", "Idea 1"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-11-11"],
+		"description" : ["Description:", "Test Description 1"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "domain"]
+	},
+	"idea6": {
+		"title" : ["Idea Title:", "Idea 2"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-12-12"],
+		"description" : ["Description:", "Test Description 2"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "website"]
+	},
+	"idea7": {
+		"title" : ["Idea Title:", "Idea 1"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-11-11"],
+		"description" : ["Description:", "Test Description 1"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "domain"]
+	},
+	"idea8": {
+		"title" : ["Idea Title:", "Idea 2"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-12-12"],
+		"description" : ["Description:", "Test Description 2"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "website"]
+	},
+	"idea9": {
+		"title" : ["Idea Title:", "Idea 1"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-11-11"],
+		"description" : ["Description:", "Test Description 1"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "domain"]
+	},
+	"idea10": {
+		"title" : ["Idea Title:", "Idea 2"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-12-12"],
+		"description" : ["Description:", "Test Description 2"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "website"]
+	},
+	"idea11": {
+		"title" : ["Idea Title:", "Idea 1"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-11-11"],
+		"description" : ["Description:", "Test Description 1"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "domain"]
+	},
+	"idea12": {
+		"title" : ["Idea Title:", "Idea 2"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-12-12"],
+		"description" : ["Description:", "Test Description 2"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "website"]
+	},
+	"idea13": {
+		"title" : ["Idea Title:", "Idea 1"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-11-11"],
+		"description" : ["Description:", "Test Description 1"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "domain"]
+	},
+	"idea14": {
+		"title" : ["Idea Title:", "Idea 2"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-12-12"],
+		"description" : ["Description:", "Test Description 2"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "website"]
+	},
+	"idea15": {
+		"title" : ["Idea Title:", "Idea 1"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-11-11"],
+		"description" : ["Description:", "Test Description 1"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "domain"]
+	},
+	"idea16": {
+		"title" : ["Idea Title:", "Idea 2"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-12-12"],
+		"description" : ["Description:", "Test Description 2"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "website"]
+	},
+	"idea17": {
+		"title" : ["Idea Title:", "Idea 1"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-11-11"],
+		"description" : ["Description:", "Test Description 1"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "domain"]
+	},
+	"idea18": {
+		"title" : ["Idea Title:", "Idea 2"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-12-12"],
+		"description" : ["Description:", "Test Description 2"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "website"]
+	},
+	"idea19": {
+		"title" : ["Idea Title:", "Idea 1"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-11-11"],
+		"description" : ["Description:", "Test Description 1"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "domain"]
+	},
+	"idea20": {
+		"title" : ["Idea Title:", "Idea 2"],
+		"importance" : ["Importance:", "6"],
+		"dateDue" : ["Date Due:", "2013-12-12"],
+		"description" : ["Description:", "Test Description 2"],
+		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "website"]
 	}
+	
 }
