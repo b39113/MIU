6e8b95283632940feae8245783112f7682dc2ee7
Boyd Tiffin <mokono@fullsail.edu>
2013-10-31 19:58:06 -0500

    Final Code Files

--- Tiffin_boyd_project1/js/json.js
+++ Tiffin_boyd_project1/js/json.js
@@ -1,162 +1,162 @@
 var json = {
 	"idea1": {
-		"title" : ["Idea Title:", "Idea 1"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "Idea Logger"],
+		"importance" : ["Importance:", "7"],
 		"dateDue" : ["Date Due:", "2013-11-11"],
-		"description" : ["Description:", "Test Description 1"],
+		"description" : ["Description:", "App that tracks ideas and phases throughout idea process"],
 		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "domain"]
+		"type" : ["Type:", "app"]
 	},
 	"idea2": {
-		"title" : ["Idea Title:", "Idea 2"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "300 Blackout"],
+		"importance" : ["Importance:", "3"],
 		"dateDue" : ["Date Due:", "2013-12-12"],
-		"description" : ["Description:", "Test Description 2"],
-		"status" : ["Status:", "Requirements"],
+		"description" : ["Description:", "Site dedicated to 300 blackout for IA"],
+		"status" : ["Status:", "Development"],
 		"type" : ["Type:", "website"]
 	},
 	"idea3": {
-		"title" : ["Idea Title:", "Idea 1"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "My House"],
+		"importance" : ["Importance:", "9"],
 		"dateDue" : ["Date Due:", "2013-11-11"],
-		"description" : ["Description:", "Test Description 1"],
-		"status" : ["Status:", "Requirements"],
+		"description" : ["Description:", "Potential future site, domain name idea now"],
+		"status" : ["Status:", "Idea"],
 		"type" : ["Type:", "domain"]
 	},
 	"idea4": {
-		"title" : ["Idea Title:", "Idea 2"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "the blood gods"],
+		"importance" : ["Importance:", "2"],
 		"dateDue" : ["Date Due:", "2013-12-12"],
-		"description" : ["Description:", "Test Description 2"],
-		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "website"]
+		"description" : ["Description:", "world of warcraft fan site"],
+		"status" : ["Status:", "Idea"],
+		"type" : ["Type:", "domain"]
 	},
 	"idea5": {
-		"title" : ["Idea Title:", "Idea 1"],
+		"title" : ["Idea Title:", "frostme"],
 		"importance" : ["Importance:", "6"],
 		"dateDue" : ["Date Due:", "2013-11-11"],
-		"description" : ["Description:", "Test Description 1"],
-		"status" : ["Status:", "Requirements"],
+		"description" : ["Description:", "diamond lovers domain"],
+		"status" : ["Status:", "Idea"],
 		"type" : ["Type:", "domain"]
 	},
 	"idea6": {
-		"title" : ["Idea Title:", "Idea 2"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "getmine"],
+		"importance" : ["Importance:", "8"],
 		"dateDue" : ["Date Due:", "2013-12-12"],
-		"description" : ["Description:", "Test Description 2"],
-		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "website"]
-	},
-	"idea7": {
-		"title" : ["Idea Title:", "Idea 1"],
-		"importance" : ["Importance:", "6"],
-		"dateDue" : ["Date Due:", "2013-11-11"],
-		"description" : ["Description:", "Test Description 1"],
+		"description" : ["Description:", "domain for government money"],
 		"status" : ["Status:", "Requirements"],
 		"type" : ["Type:", "domain"]
 	},
-	"idea8": {
-		"title" : ["Idea Title:", "Idea 2"],
-		"importance" : ["Importance:", "6"],
-		"dateDue" : ["Date Due:", "2013-12-12"],
-		"description" : ["Description:", "Test Description 2"],
+	"idea7": {
+		"title" : ["Idea Title:", "shopallday"],
+		"importance" : ["Importance:", "7"],
+		"dateDue" : ["Date Due:", "2013-11-11"],
+		"description" : ["Description:", "shopping cart site"],
 		"status" : ["Status:", "Requirements"],
+		"type" : ["Type:", "website"]
+	},
+	"idea8": {
+		"title" : ["Idea Title:", "mymanlyroom"],
+		"importance" : ["Importance:", "3"],
+		"dateDue" : ["Date Due:", "2013-12-12"],
+		"description" : ["Description:", "home theatre site for DIY people"],
+		"status" : ["Status:", "Development"],
 		"type" : ["Type:", "website"]
 	},
 	"idea9": {
-		"title" : ["Idea Title:", "Idea 1"],
+		"title" : ["Idea Title:", "golightly"],
 		"importance" : ["Importance:", "6"],
 		"dateDue" : ["Date Due:", "2013-11-11"],
-		"description" : ["Description:", "Test Description 1"],
-		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "domain"]
+		"description" : ["Description:", "gun cleaning oil that is scent free"],
+		"status" : ["Status:", "Development"],
+		"type" : ["Type:", "website"]
 	},
 	"idea10": {
-		"title" : ["Idea Title:", "Idea 2"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "snakesnare"],
+		"importance" : ["Importance:", "5"],
 		"dateDue" : ["Date Due:", "2013-12-12"],
-		"description" : ["Description:", "Test Description 2"],
-		"status" : ["Status:", "Requirements"],
+		"description" : ["Description:", "website to sell snake snaring tools"],
+		"status" : ["Status:", "Development"],
 		"type" : ["Type:", "website"]
 	},
 	"idea11": {
-		"title" : ["Idea Title:", "Idea 1"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "diecasteverything"],
+		"importance" : ["Importance:", "3"],
 		"dateDue" : ["Date Due:", "2013-11-11"],
-		"description" : ["Description:", "Test Description 1"],
+		"description" : ["Description:", "a site dedicated to all things diecast"],
 		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "domain"]
+		"type" : ["Type:", "website"]
 	},
 	"idea12": {
-		"title" : ["Idea Title:", "Idea 2"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "myownracer"],
+		"importance" : ["Importance:", "3"],
 		"dateDue" : ["Date Due:", "2013-12-12"],
-		"description" : ["Description:", "Test Description 2"],
+		"description" : ["Description:", "site dedicated to club level motorcycle racing"],
 		"status" : ["Status:", "Requirements"],
 		"type" : ["Type:", "website"]
 	},
 	"idea13": {
-		"title" : ["Idea Title:", "Idea 1"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "clean my gun"],
+		"importance" : ["Importance:", "2"],
 		"dateDue" : ["Date Due:", "2013-11-11"],
-		"description" : ["Description:", "Test Description 1"],
-		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "domain"]
+		"description" : ["Description:", "app that gives step by step instructions on safe gun cleaning"],
+		"status" : ["Status:", "Development"],
+		"type" : ["Type:", "app"]
 	},
 	"idea14": {
-		"title" : ["Idea Title:", "Idea 2"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "my pool"],
+		"importance" : ["Importance:", "1"],
 		"dateDue" : ["Date Due:", "2013-12-12"],
-		"description" : ["Description:", "Test Description 2"],
-		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "website"]
+		"description" : ["Description:", "app that takes your pool size and tells you what chemicals you need based on ph levels"],
+		"status" : ["Status:", "Production"],
+		"type" : ["Type:", "app"]
 	},
 	"idea15": {
-		"title" : ["Idea Title:", "Idea 1"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "Home Maintenance"],
+		"importance" : ["Importance:", "9"],
 		"dateDue" : ["Date Due:", "2013-11-11"],
-		"description" : ["Description:", "Test Description 1"],
-		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "domain"]
+		"description" : ["Description:", "app that tracks your home and what it needs daily, not just a task tracker"],
+		"status" : ["Status:", "Idea"],
+		"type" : ["Type:", "app"]
 	},
 	"idea16": {
-		"title" : ["Idea Title:", "Idea 2"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "political tower game"],
+		"importance" : ["Importance:", "7"],
 		"dateDue" : ["Date Due:", "2013-12-12"],
-		"description" : ["Description:", "Test Description 2"],
-		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "website"]
+		"description" : ["Description:", "liberal vs conservative tower defense game where you destroy the other party"],
+		"status" : ["Status:", "Production"],
+		"type" : ["Type:", "app"]
 	},
 	"idea17": {
-		"title" : ["Idea Title:", "Idea 1"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "Tower Bees"],
+		"importance" : ["Importance:", "4"],
 		"dateDue" : ["Date Due:", "2013-11-11"],
-		"description" : ["Description:", "Test Description 1"],
-		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "domain"]
+		"description" : ["Description:", "Based on Bees vs ants tower game. improve controls and do different characters"],
+		"status" : ["Status:", "Production"],
+		"type" : ["Type:", "app"]
 	},
 	"idea18": {
-		"title" : ["Idea Title:", "Idea 2"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "Spaffin"],
+		"importance" : ["Importance:", "2"],
 		"dateDue" : ["Date Due:", "2013-12-12"],
-		"description" : ["Description:", "Test Description 2"],
-		"status" : ["Status:", "Requirements"],
+		"description" : ["Description:", "deal site similiar to dealnews except a better layout and we do shopping for people"],
+		"status" : ["Status:", "Idea"],
 		"type" : ["Type:", "website"]
 	},
 	"idea19": {
-		"title" : ["Idea Title:", "Idea 1"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "Rooted Home"],
+		"importance" : ["Importance:", "5"],
 		"dateDue" : ["Date Due:", "2013-11-11"],
-		"description" : ["Description:", "Test Description 1"],
-		"status" : ["Status:", "Requirements"],
-		"type" : ["Type:", "domain"]
+		"description" : ["Description:", "Website dedicated to homeopathic medicine"],
+		"status" : ["Status:", "Production"],
+		"type" : ["Type:", "website"]
 	},
 	"idea20": {
-		"title" : ["Idea Title:", "Idea 2"],
-		"importance" : ["Importance:", "6"],
+		"title" : ["Idea Title:", "Get Some Guns Store"],
+		"importance" : ["Importance:", "5"],
 		"dateDue" : ["Date Due:", "2013-12-12"],
-		"description" : ["Description:", "Test Description 2"],
-		"status" : ["Status:", "Requirements"],
+		"description" : ["Description:", "develop site for GetSomeGunsStore.com, upgradable to shopping cart"],
+		"status" : ["Status:", "Production"],
 		"type" : ["Type:", "website"]
 	}
 	
